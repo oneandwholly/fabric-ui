502944985af289e98679d1114a0f60a6eb36bba8
Jon Huh <jonhuh@Jons-MacBook-Pro.local>
2019-05-26 20:32:38 +0900

    add socket

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import io from 'socket.io-client'
+const socket = io();
+
+io.on('connection', (socket) => {
+    console.log('connected')
+  });
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
