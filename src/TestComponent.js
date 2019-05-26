f57f38b67f2c7200ef2d1d247fbcc1fbbfc6c1fd
Jon Huh <jonhuh@Jons-MacBook-Pro.local>
2019-05-26 22:46:55 +0900

    test

--- src/TestComponent.js
+++ src/TestComponent.js
@@ -18,6 +18,29 @@ export default class extends Component {
             if (type === 'pod-status-event') {
                 console.log({ payload })
             }
+
+            const event = payload;
+            const eventType = event.type
+            const podName = event.object.metadata.name
+
+            if (eventType === 'ADDED') {
+                this.setState({
+                    podStatus: { ...this.state.podStatus, [podName]: [event]}
+                })
+              } else {
+                this.setState({
+                    podStatus: { ...this.state.podStatus, [podName]: [...this.state.podStatus[podName], event]}
+                })
+              }
+          
+              if (eventType === 'DELETED') {
+                const podStatusCopy = { ...this.state.podStatus }
+                delete podStatusCopy[podName]
+
+                this.setState({
+                    podStatus: podStatusCopy
+                })
+              }
         })
     }
     
